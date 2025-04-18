9ae5bb7c2c7f51cc300f8a9035a422867fe3eea4
Rehtt <dsreshiram@gmail.com>
2025-04-18 15:48:07 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Rehtt/registerHost
+
+go 1.24.2
+
+require (
+	github.com/Rehtt/Kit/web v0.0.0-20250328063957-96290e7d9b71 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	goweb "github.com/Rehtt/Kit/web"
+)
+
+var (
+	lock       sync.Mutex
+	listenAddr = flag.String("listen", "127.0.0.1:8080", "listen addr")
+)
+
+func main() {
+	flag.Parse()
+
+	g := goweb.New()
+	g.GET("/set/host/#host", func(ctx *goweb.Context) {
+		host := ctx.GetUrlPathParam("host")
+		remoteAddr := ctx.Request.RemoteAddr
+		ip := strings.Split(remoteAddr, ":")[0]
+		setSystemHost(host, net.ParseIP(ip))
+	})
+	if err := http.ListenAndServe(*listenAddr, g); err != nil {
+		slog.Error("listen failed", "err", err)
+	}
+}
+
+func setSystemHost(host string, ip net.IP) {
+	if ip == nil {
+		return
+	}
+	lock.Lock()
+	defer lock.Unlock()
+
+	hosts, err := os.ReadFile("/etc/hosts")
+	if err != nil {
+		slog.Error("set host failed", "err", err)
+		return
+	}
+	data := strings.Split(string(hosts), "\n")
+	var find bool
+	for i := range data {
+		v := strings.ReplaceAll(data[i], "\t", " ")
+		v = strings.TrimSpace(v)
+		if strings.HasSuffix(v, " "+host) {
+			data[i] = ip.String() + " " + host
+			find = true
+			break
+		}
+	}
+
+	if !find {
+		data = append(data, ip.String()+" "+host)
+	}
+
+	info, _ := os.Stat("/etc/hosts")
+	if err = os.WriteFile("/etc/hosts", []byte(strings.Join(data, "\n")), info.Mode()); err != nil {
+		slog.Error("set host failed", "err", err)
+	}
+}
